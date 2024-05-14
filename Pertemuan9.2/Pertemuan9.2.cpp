8fc00dc489279945b5e36b05eb3ed7255032efc9
LichtPUBLIC <daffaalyandra65@gmail.com>
2024-05-14 13:25:29 +0700

    mbrStatikFunc

---
+++ Pertemuan9.2/Pertemuan9.2.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printALL();
+	
+	static void setNim(int pNim) { nim = pNim; /*Definisi Function*/ }
+	static int getNim() { return nim; /*Definisi Function*/ }
+
+	mahasiswa(string  pnama) :nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 172; 
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printALL() {
+	cout << "ID   = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+}
+
+int main() {
+	mahasiswa mhs1("Sri Dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+	mahasiswa::setNim(9); //mengakses nim melalui ststic member function "setNim"
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wahono");
+
+	mhs1.printALL();
+	mhs2.printALL();
+	mhs3.printALL();
+	mhs4.printALL();
+
+	cout << "akses dari luar object = " << mahasiswa::getNim() << endl; //mengakses nim
+	system("pause");
+
+	return 0;
+} 
---
+++ Pertemuan9.3/Pertemuan9.3.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	int nim;
+	string nama;
+public:
+	mahasiswa() {
+		nim = 0;
+		nama = "";
+	};
+
+	mahasiswa(int iNim) {
+		nim = iNim;
+	};
+	mahasiswa(string);
+	mahasiswa(int iNim, string iNama);
+	void cetak();
+};
+
+mahasiswa::mahasiswa(string iNama) {
+	nim = 0;
+	nama = iNama;
+}
+
+mahasiswa::mahasiswa(int iNim, string iNama) {
+	nim = iNim;
+	nama = iNama;
+}
+void mahasiswa::cetak() {
+	cout << endl << "Nim =" << nim << endl;
+	cout << "Nama = " << nama << endl;
+}
+
+int main() {
+	mahasiswa daoa; 
+	mahasiswa pema(65);
+	mahasiswa lodon ("Dutdut");
+	mahasiswa mega (69, "Ariru");
+
+	daoa.cetak();
+	pema.cetak();
+	lodon.cetak();
+	mega.cetak();
+
+		system("pause");
+
+	return 0;
+}
